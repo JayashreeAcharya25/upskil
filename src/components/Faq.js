b21d30c4eded60c896ceda2f27123c9e58272771
JayashreeAcharya25 <jayaacharya1997@gmail.com>
2024-01-07 22:37:30 +0530

    class renamed to classname

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -23,69 +23,69 @@ export default class Faq extends Component {
               </div>
             </div>
             <div className="block-elements">
-              <div class="accordion accordion-flush" id="accordionFlushExample">
+              <div className="accordion accordion-flush" id="accordionFlushExample">
 
-                <div class="accordion-item">
-                  <h2 class="accordion-header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                <div className="accordion-item">
+                  <h2 className="accordion-header">
+                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
                       How do I enrol in a course?
                     </button>
                   </h2>
-                  <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                    <div class="accordion-body">Simple, Just click on the course you're interested in, click on the "Enrol Now" button, and
+                  <div id="flush-collapseOne" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                    <div className="accordion-body">Simple, Just click on the course you're interested in, click on the "Enrol Now" button, and
                       follow the instructions to create an account or log in. Once you've completed the payment,
                       you will get an immediate access to course you enrolled for.
                     </div>
                   </div>
                 </div>
 
-                <div class="accordion-item">
-                  <h2 class="accordion-header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                <div className="accordion-item">
+                  <h2 className="accordion-header">
+                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
                       Is the course explained in Kannada or English?
                     </button>
                   </h2>
-                  <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                    <div class="accordion-body">The course content will be explained in kannada with simple
+                  <div id="flush-collapseTwo" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                    <div className="accordion-body">The course content will be explained in kannada with simple
                       examples and exercises which you can practise at home. </div>
                   </div>
                 </div>
 
-                <div class="accordion-item">
-                  <h2 class="accordion-header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
+                <div className="accordion-item">
+                  <h2 className="accordion-header">
+                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseThree" aria-expanded="false" aria-controls="flush-collapseThree">
                       Are these courses Pre-recorded or Live?
                     </button>
                   </h2>
-                  <div id="flush-collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                    <div class="accordion-body">All our courses are Pre-recorded. This means you can learn these
+                  <div id="flush-collapseThree" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                    <div className="accordion-body">All our courses are Pre-recorded. This means you can learn these
                       courses at your own schedule anywhere anytime. You'll have access to
                       the course 24/7 for 1 complete year.
                     </div>
                   </div>
                 </div>
 
-                <div class="accordion-item">
-                  <h2 class="accordion-header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
+                <div className="accordion-item">
+                  <h2 className="accordion-header">
+                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFour" aria-expanded="false" aria-controls="flush-collapseFour">
                       Will I get a certificate of completion?
                     </button>
                   </h2>
-                  <div id="flush-collapseFour" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                    <div class="accordion-body">Yes! Each course you successfully complete will earn you a certificate of
+                  <div id="flush-collapseFour" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                    <div className="accordion-body">Yes! Each course you successfully complete will earn you a certificate of
                       achievement.
                     </div>
                   </div>
                 </div>
 
-                <div class="accordion-item">
-                  <h2 class="accordion-header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFive" aria-expanded="false" aria-controls="flush-collapseFive">
+                <div className="accordion-item">
+                  <h2 className="accordion-header">
+                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseFive" aria-expanded="false" aria-controls="flush-collapseFive">
                       Can I cancel my course purchase if I don’t like it? Will I get a refund?
                     </button>
                   </h2>
-                  <div id="flush-collapseFive" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                    <div class="accordion-body">Yes..!! Your satisfaction is very important for us, You can cancel your course
+                  <div id="flush-collapseFive" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                    <div className="accordion-body">Yes..!! Your satisfaction is very important for us, You can cancel your course
                       within 7 days of purchase if you don’t like it. You can email us at
                       upskil.work@gmail.com & we’ll cancel and refund your money within 3
                       working days. No questions asked!
@@ -93,40 +93,40 @@ export default class Faq extends Component {
                   </div>
                 </div>
 
-                <div class="accordion-item">
-                  <h2 class="accordion-header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSix" aria-expanded="false" aria-controls="flush-collapseSix">
+                <div className="accordion-item">
+                  <h2 className="accordion-header">
+                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSix" aria-expanded="false" aria-controls="flush-collapseSix">
                       Is it a communication course or an English-speaking course?
                     </button>
                   </h2>
-                  <div id="flush-collapseSix" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                    <div class="accordion-body">These Courses are designed to improve both your communication skill And English speaking
+                  <div id="flush-collapseSix" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                    <div className="accordion-body">These Courses are designed to improve both your communication skill And English speaking
                       skill.
                     </div>
                   </div>
                 </div>
 
-                <div class="accordion-item">
-                  <h2 class="accordion-header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSeven" aria-expanded="false" aria-controls="flush-collapseSeven">
+                <div className="accordion-item">
+                  <h2 className="accordion-header">
+                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseSeven" aria-expanded="false" aria-controls="flush-collapseSeven">
                       Can I see a sample of course before enrolling?
                     </button>
                   </h2>
-                  <div id="flush-collapseSeven" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                    <div class="accordion-body">Yes Definitely! You will get free previews for selected lessons within our courses, to get a
+                  <div id="flush-collapseSeven" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                    <div className="accordion-body">Yes Definitely! You will get free previews for selected lessons within our courses, to get a
                       glimpse of the content and teaching style before you enrol in the course.
                     </div>
                   </div>
                 </div>
 
-                <div class="accordion-item">
-                  <h2 class="accordion-header">
-                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseEight" aria-expanded="false" aria-controls="flush-collapseEight">
+                <div className="accordion-item">
+                  <h2 className="accordion-header">
+                    <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseEight" aria-expanded="false" aria-controls="flush-collapseEight">
                       How to report an issue?
                     </button>
                   </h2>
-                  <div id="flush-collapseEight" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                    <div class="accordion-body">For any issues or queries, please drop an email to upskil.work@gmail.com with the
+                  <div id="flush-collapseEight" className="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                    <div className="accordion-body">For any issues or queries, please drop an email to upskil.work@gmail.com with the
                       complete issue details.
                     </div>
                   </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,24 @@
 import React from 'react'
 import '../core_ui/NavBar.css'
-import {Link} from 'react-router-dom'
 
 const NavBar = () => {
     return (
         <div className="navbar">
             <div className="navbar-top">
                 <div className="section">
-                    <img src="https://upskilwebpagebucket.s3.ap-south-1.amazonaws.com/Upskil+logo+cropped.jpeg" alt="logo" />
+                    <a href="/"><img src="https://upskilwebpagebucket.s3.ap-south-1.amazonaws.com/Upskil+logo+cropped.jpeg" alt="logo" /></a>
                 </div>
                 <div className="section">
-                    <input type="checkbox" id="menu-toggle" class="menu-toggle" />
-                    <label for="menu-toggle" class="menu-icon">&#9776;</label>
+                    <input type="checkbox" id="menu-toggle" className="menu-toggle" />
+                    <label htmlFor="menu-toggle" className="menu-icon">&#9776;</label>
                     <ul className='nav-list'>
                         <li className="nav-link"><a href="/">Courses</a></li>
                         <li className="nav-link"><a href="/">Blog</a></li>
                         <li className="nav-link"><a href="/">About</a></li>
                         <li className="nav-link"><a href="/">Digital Products</a></li>
                         <li className="nav-link">
-                            <Link className='desktop-icon' to="/"><img className='shopping-cart' src="https://upskilwebpagebucket.s3.ap-south-1.amazonaws.com/shopping_cart_icon.png" alt="" /></Link>
-                            <Link className='mobile-icon' to="/"><img className='shopping-cart' src="https://upskilwebpagebucket.s3.ap-south-1.amazonaws.com/shopping_cart_icon.png" alt="" />Cart</Link></li>
+                            <a className='desktop-icon' href="/"><img className='shopping-cart' src="https://upskilwebpagebucket.s3.ap-south-1.amazonaws.com/shopping_cart_icon.png" alt="" /></a>
+                            <a className='mobile-icon' href="/"><img className='shopping-cart' src="https://upskilwebpagebucket.s3.ap-south-1.amazonaws.com/shopping_cart_icon.png" alt="" />Cart</a></li>
                         <li className="nav-link"><button className='btn nav-btn'>Login</button></li>
                     </ul>
                 </div>
