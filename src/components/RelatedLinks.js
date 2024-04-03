790224cd71cb90ae20d000da2ab22b60018a6ac0
JayashreeAcharya25 <jayaacharya1997@gmail.com>
2024-04-03 09:45:39 +0530

    Added support and refund policy link

--- src/components/RelatedLinks.js
+++ src/components/RelatedLinks.js
@@ -10,8 +10,8 @@ export default class RelatedLinks extends Component {
                         <li className='links'><h3>Related Links</h3></li>
                         <li className='links'><a href='https://courses.upskil.work/aboutus'>About Us</a></li>
                         <li className='links'><a href='https://courses.upskil.work/blog'>Blogs</a></li>
-                        <li className='links'><a href='https://www.upskil.work/contactus'>Support</a></li>
-                        <li className='links'><a href='https://www.upskil.work/refundpolicy'>Refund Policy</a></li>
+                        <li className='links'><a href='mailto:upskil.help@gmail.com'>Support</a></li>
+                        <li className='links'><a href='https://courses.upskil.work/refundpolicy'>Refund Policy</a></li>
                     </ul>
                 </div>
             </div>
