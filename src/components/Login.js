96708bc278900a5161acced67e3b6bca304fffc9
JayashreeAcharya25 <jayaacharya1997@gmail.com>
2024-04-10 23:20:49 +0530

    Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -21,7 +21,7 @@ export default function Login({ toggleModal }) {
 
               <div className="modal-body-content mt-4">
                 <div className="modal-buttons  d-flex flex-column gap-3">
-                  <a className="d-flex justify-content-center align-items-center gap-2" href="/"> <img className="img-fluid" src={google_icon} alt="google"/>  Continue with Google</a>
+                  <a className="d-flex justify-content-center align-items-center gap-2" href="https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?redirect_uri=https%3A%2F%2Fauthenticate.ongraphy.com%2Ft%2Fpublic%2Fgoogle%2Fredirect&response_type=code%20id_token&scope=email%20profile%20openid&openid.realm&include_granted_scopes=true&client_id=771122676243-cb11avd40vp8p9mblpks64a2efll2jou.apps.googleusercontent.com&ss_domain=https%3A%2F%2Fauthenticate.ongraphy.com&fetch_basic_profile=true&gsiwebsdk=2&flowName=GeneralOAuthFlow&prompt=select_account&state=https%3A%2F%2Fcourses.upskil.work%7C%7C%7B%22source%22%3A%22digitalProductStore%22%2C%22platform%22%3A%22web%22%2C%22sourceId%22%3A%22%22%7D%7C%7Cnull%7C%7Cnull%7C%7Cnull&service=lso&o2v=1&theme=mn&ddm=0"> <img className="img-fluid" src={google_icon} alt="google"/>  Continue with Google</a>
                   <a className="d-flex justify-content-center align-items-center gap-2" href="/"> <img className="img-fluid" src={mail_icon} alt="google"/> Continue with email</a>
                 </div>
               </div>
